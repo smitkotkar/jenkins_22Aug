c9636e4dab11e95e3946552fe7a3861bdd0fe440
smit <smitkotkar42@gmail.com>
2023-08-22 14:33:06 +0530

    test with sonarwubeEnv

--- studentapp.groovy
+++ studentapp.groovy
@@ -11,9 +11,16 @@ pipeline {
                 sh '/opt/apache-maven/bin/mvn clean package'
             }
         }
-        stage ('Test') {
+        // stage ('Test') {
+        //     steps {
+        //         sh '/opt/apache-maven/bin/mvn sonar:sonar   -Dsonar.projectKey=studentapp   -Dsonar.host.url=http://54.173.83.49:9000   -Dsonar.login=3a3d798cc376e4c2efb54b716c4104106efe6bf9'
+        //     }
+        // }
+        stage ('test') {
             steps {
-                sh '/opt/apache-maven/bin/mvn sonar:sonar   -Dsonar.projectKey=studentapp   -Dsonar.host.url=http://54.173.83.49:9000   -Dsonar.login=3a3d798cc376e4c2efb54b716c4104106efe6bf9'
+                withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar_token') { 
+                  sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=studentapp'
+               }
             }
         }
         stage ('Deploy') {
