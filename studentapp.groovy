6faa27fbf118ce2dee66591a122eee79c2906c9a
smit <smitkotkar42@gmail.com>
2023-08-22 14:44:15 +0530

    DDas

--- studentapp.groovy
+++ studentapp.groovy
@@ -16,7 +16,7 @@ pipeline {
         //         sh '/opt/apache-maven/bin/mvn sonar:sonar   -Dsonar.projectKey=studentapp   -Dsonar.host.url=http://54.173.83.49:9000   -Dsonar.login=3a3d798cc376e4c2efb54b716c4104106efe6bf9'
         //     }
         // }
-        stage ('test') {
+        stage ('test-built') {
             steps {
                 withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar_token') { 
                   sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=studentapp'
