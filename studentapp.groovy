fa73db1536f4caeb7bd385fdd9e2d4147fbc34ee
smit <smitkotkar42@gmail.com>
2023-08-22 13:09:39 +0530

    build stage update

--- studentapp.groovy
+++ studentapp.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage ('Build') {
             steps {
-                echo 'Build succeed'
+                sh '/opt/apache-maven/bin/mvn clean package'
             }
         }
         stage ('Test') {
